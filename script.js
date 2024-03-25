5b5b8e83bc753b519ca6baa723038b4a648619f7
Meekaylah <kaylahusanga@gmail.com>
2024-03-26 00:38:14 +0100

    Removed punctuations and cleared textInput onClick

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ const result = document.getElementById("result");
 
 const checkWord = (text) => {
     const string = text.value;
-    const lcase = string.toLowerCase();
+    const punctuationless = string.replace(/[^\p{L}\p{N}\s]/gu,"") .replace(/\s+/g, "");
+    const lcase = punctuationless.toLowerCase();
     const reverse = [...lcase].reverse().join('');
-    const pal = `<p class = user-input> <strong>${string}</strong> is a palindrome</p>`;
-    const npal = `<p class = user-input> <strong>${string}</strong> is not a palindrome</p>`;
+    const pal = `<p class = user-input> <strong>${string}</strong> is a palindrome.</p>`;
+    const npal = `<p class = user-input> <strong>${string}</strong> is not a palindrome.</p>`;
     result.classList.remove("hidden");
-    reverse === lcase ? result.innerHTML = pal : result.innerHTML = npal; 
+    reverse === lcase ? result.innerHTML = pal : result.innerHTML = npal;
+    textInput.value = "";
 }
 
 checkBtn.addEventListener("click", () => {
     if (textInput.value === "") {
         alert("Please input a value")
+        textInput.value = "";
     } else {
         checkWord(textInput)
     }
