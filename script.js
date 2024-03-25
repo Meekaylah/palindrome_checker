43b073f242621ba417d0bca630e34060bf55480d
Meekaylah <kaylahusanga@gmail.com>
2024-03-25 23:42:01 +0100

    Figured out palindrome logic

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input");
 const result = document.getElementById("result");
+
+
+const checkWord = (text) => {
+    const string = text.value;
+    const lcase = string.toLowerCase();
+    const reverse = [...lcase].reverse().join('');
+    reverse === lcase ? alert(`${string} is a palindrome`) : alert(`${string} is NOT a palindrome`);
+}
+
+checkBtn.addEventListener("click", () => {
+    if (textInput.value === "") {
+        alert("Please input a value")
+    } else {
+        checkWord(textInput)
+    }
+})
